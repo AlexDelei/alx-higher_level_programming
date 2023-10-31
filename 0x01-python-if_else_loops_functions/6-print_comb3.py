9646cb51051edf322c146ae92894c4c2a9e577f9
AlexDelei <alexdelei125@gmail.com>
2023-10-31 15:33:04 -0700

    if/else, loops, functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,10 +2,6 @@
 for i in range(10):
     for j in range(i + 1, 10):
         if i == 0:
-            print("0{:1d}, ".format(j), end="")
+            print("0{:1d}, ".format(j), end="" if j != 9 else "\n")
         else:
-            print("{:02d}, ".format(i * 10 + j), end="")
-        if i != 8 or j != 9:
-            print("", end="")
-        else:
-            print("\n", end="")
+            print("{:02d}, ".format(i * 10 + j), end="" if i != 8 or j != 9 else "\n")
