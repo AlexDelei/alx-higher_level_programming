581354f8a2f1f29b725af800f1d5f8be374d20d9
AlexDelei <alexdelei125@gmail.com>
2023-10-31 12:33:13 -0700

    if/else, loops, functions

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i < 10:
-        print(f"0{i}", end=", ")
+        print("{:2d}".format(i), end=", ")
     else:
-        print(i, end=", ")
+        print("{:d}".format(99)
