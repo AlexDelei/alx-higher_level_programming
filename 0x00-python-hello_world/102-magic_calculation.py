e01927116cf85217aacb2f51f1bc2a307107d2d4
AlexDelei <alexdelei125@gmail.com>
2023-11-01 10:14:35 -0700

    bytecode

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    result = 98 ** a  # Exponentiation: 98 to the power of 'a'
-    result += b  # Addition: Add 'b' to the result
-    return result
+    return a + 98 ** b
