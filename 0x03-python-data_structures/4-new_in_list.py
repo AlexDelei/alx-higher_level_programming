c14b2f6af61a4dddf5e1ade4b6c5211da88de564
AlexDelei <alexdelei125@gmail.com>
2023-11-04 13:45:01 -0700

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,9 +3,9 @@ def new_in_list(my_list, idx, element):
     my_list_ = my_list[:]
     len_ = len(my_list)
 
-    if idx < 0 or idx > len_:
-        return my_list
+    if idx < 0 or idx >= len_:
+        return my_list[:]
     else:
-        my_list_.insert(idx, element)
-        my_list_.pop(idx + 1)
-        return my_list_
+        new_list = my_list[:]
+        new_list[idx] = element
+        return new_list
