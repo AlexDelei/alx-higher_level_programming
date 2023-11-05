611eaca25cea3e9c9bf1d983bd351ce7c430b8a1
AlexDelei <alexdelei125@gmail.com>
2023-11-05 04:59:40 -0800

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,22 +2,23 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     i = (0,)
     j = (0, 0)
-    if len(tuple_a) == 1:
-        tuple_a = tuple_a + i
-        t_a = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-        return t_a
-    elif len(tuple_b) == 1:
-        tuple_b = tuple_b + i
-        t_a = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-        return t_a
-    elif len(tuple_a) == 0:
-        tuple_a = j
-        t_a = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-        return t_a
-    elif len(tuple_b) == 0:
-        tuple_b = j
-        t_a = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-        return t_a
-    else:
-        t_a = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-        return t_a
+    if len(tuple_a) != 3 and len(tuple_b) != 3:
+        if len(tuple_a) == 1:
+            tuple_a = tuple_a + i
+            t_a = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+            return t_a
+        elif len(tuple_b) == 1:
+            tuple_b = tuple_b + i
+            t_a = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+            return t_a
+        elif len(tuple_a) == 0:
+            tuple_a = j
+            t_a = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+            return t_a
+        elif len(tuple_b) == 0:
+            tuple_b = j
+            t_a = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+            return t_a
+        else:
+            t_a = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+            return t_a
