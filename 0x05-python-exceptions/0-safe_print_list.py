c9ec5951624f71adae95b2ffa2962297bda9cab0
AlexDelei <alexdelei125@gmail.com>
2023-11-20 01:45:33 -0800

    Exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for i in my_list:
             cnt += 1
         for i in range(x):
-            print("{:d}".format(my_list[i]), end="")
+            print(my_list[i], end="")
         print()
         return x
     except:
