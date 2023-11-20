e6310b0dc3f6998e16893c41d318b4697cac1fa9
AlexDelei <alexdelei125@gmail.com>
2023-11-20 12:34:24 -0800

    Exceptions

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -7,7 +7,7 @@ def list_division(my_list_1, my_list_2, list_length):
 
         try:
             element_1 = my_list_1[i] if i < len(my_list_1) else 0
-            element_2 = my_list_2[i] if i < len(my_list_2) and my_list_2[i] != 0 else 0
+            element_2 = my_list_2[i] if i < len(my_list_2) else 0
 
             result = element_1 / element_2
 
