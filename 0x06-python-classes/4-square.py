4ac543376eceaa30341d7bfce6e3f42b117509c1
AlexDelei <alexdelei125@gmail.com>
2023-11-21 12:13:28 -0800

    Python - Classes and Objects

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -51,6 +51,7 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
         """
         Calculate and return the area of the square.
