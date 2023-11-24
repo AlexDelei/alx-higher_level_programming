5455179cf10695cbcf27127967c4cab2057ce91f
AlexDelei <alexdelei125@gmail.com>
2023-11-24 10:35:45 -0800

    Test-driven

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module for an addition function
+>>> add_integer(1)
+99
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Adds two integers
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return a + b
