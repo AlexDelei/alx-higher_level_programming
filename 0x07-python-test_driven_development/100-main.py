9b006f90b3e9fdff3ad88e042607653b36fdbc81
AlexDelei <alexdelei125@gmail.com>
2023-11-27 01:21:46 -0800

    Test-driven

--- 0x07-python-test_driven_development/100-main.py
+++ 0x07-python-test_driven_development/100-main.py
@@ -2,4 +2,13 @@
 matrix_mul = __import__('100-matrix_mul').matrix_mul
 
 print(matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
-print(matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
+print(matrix_mul([[5]], [[2]]))
+print(matrix_mul([[1.0, 2, 3.5], [4, 5.5, 6]], [[7, 8.2], [9, 10], [11.1, 12]]))
+print(matrix_mul([[1, 2], [3, 4]], [[5, 6, 7], [8, 9, 10]]))
+print(matrix_mul([[1, 2], [3, 4]], [[5, 6, 7], [8, 9, 10]]))
+print(matrix_mul([[1, 2, 3], [4, 5, 6]], [[7, 8], [9, 10], [11, 12]]))
+print(matrix_mul([['a', 'b'], ['c', 'd']], [['e', 'f'], ['g', 'h']]))
+print(matrix_mul([[0]], [[34]]))
+print(matrix_mul([[1, 2, 'a'], [4, 5, 6]], [[7, 8], [9, 10], [11, 12]]))
+print(matrix_mul([[1, 2, 3], [4, 5, 6], [7, 8]], [[9, 10], [11, 12]]))
+print(matrix_mul([[]], [[]]))
--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -8,6 +8,12 @@ def matrix_mul(m_a, m_b):
     """
 
     """
+    if not m_a or not any(m_a):
+        raise ValueError("m_a can't be empty")
+    if not m_b or not any(m_b):
+        raise ValueError("m_b can't be empty")
+    if not m_a or not m_b or not any(m_a) or not any(m_b):
+        raise ValueError("m_a can't be empty and m_b can't be empty")
     if not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
     if not isinstance(m_b, list):
@@ -16,10 +22,6 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_a must be a list of lists")
     if not all(isinstance(row, list) for row in m_b):
         raise TypeError("m_b must be a list of lists")
-    if not m_a:
-        raise ValueError("m_a can't be empty")
-    if not m_b:
-        raise ValueError("m_b can't be empty")
     if not all(isinstance(element, (int, float)) for row in m_a for element in row):
         raise TypeError("m_a should contain only integers or floats")
     if not all(isinstance(element, (int, float)) for row in m_b for element in row):
