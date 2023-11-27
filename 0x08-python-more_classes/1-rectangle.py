dcbe28615d032c65477c54302514bd6452f9865d
AlexDelei <alexdelei125@gmail.com>
2023-11-27 12:05:53 -0800

    More Classes and Objects

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,14 +3,33 @@
 
 
 class Rectangle:
+    """
+    Initializing the program
+    
+    parameters:
+    width (int) - the width of the rect
+    height(int) - The height of the rect
+
+    """
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+    
+    """
+    width assigninment
 
+    returns:
+    the size
+    """
     @property
     def width(self):
         return self.__width
+    """
+    width assigninment
 
+    returns:
+    the size
+    """
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -18,11 +37,21 @@ class Rectangle:
         elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+    """
+    width assigninment
 
+    returns:
+    the size
+    """
     @property
     def height(self):
         return self.__height
+    """
+    width assigninment
 
+    returns:
+    the size
+    """
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
