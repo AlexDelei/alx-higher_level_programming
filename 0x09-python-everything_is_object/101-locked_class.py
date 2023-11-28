aac8f62608db34dd41ed89a866a058a7e9290789
AlexDelei <alexdelei125@gmail.com>
2023-11-28 10:48:25 -0800

    Everything is object

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+"""Locked Class """
 
+
+class LockedClass:
+    __slots__ = ['first_name']
+    """
+    A simple example of locked object
+    """
+    def __init__(self):
+        pass
