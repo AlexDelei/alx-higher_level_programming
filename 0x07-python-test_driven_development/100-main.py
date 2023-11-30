15f6a61ac14d10c12bf55ac83de1a7d48d6aafa5
AlexDelei <alexdelei125@gmail.com>
2023-11-29 21:39:33 -0800

    Test-driven

--- 0x07-python-test_driven_development/100-main.py
+++ 0x07-python-test_driven_development/100-main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 matrix_mul = __import__('100-matrix_mul').matrix_mul
 
-print(matrix_mul())
+print(matrix_mul([[5, 6, 10], [7, 8]], [[5, 6], [7, 8]]))
 print(matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
 print(matrix_mul([[5]], [[2]]))
 print(matrix_mul([[1.0, 2, 3.5], [4, 5.5, 6]], [[7, 8.2], [9, 10], [11.1, 12]]))
