d315c90e53fdbd80452e83010700d7fd8217ce6a
AlexDelei <alexdelei125@gmail.com>
2023-12-05 05:04:20 -0800

    file handling in python

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Adds items from arguments"""
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+existing_data = load_from_json_file(filename)
+
+# Add the command-line arguments to the list
+new_data = existing_data + sys.argv[1:]
+
+# Save the updated list to the JSON file
+save_to_json_file(new_data, filename)
