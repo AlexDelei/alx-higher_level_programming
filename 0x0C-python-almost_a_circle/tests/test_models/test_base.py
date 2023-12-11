8f0549f2e70cf195869be51932a540f872a1cf46
AlexDelei <alexdelei125@gmail.com>
2023-12-11 14:26:56 -0800

    python is almost a circle

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import unittest
+import os
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
@@ -28,6 +29,7 @@ class TestBase(unittest.TestCase):
         obj = Base(id=-5)
         self.assertEqual(obj.id, -5)
 
+
     def test_create_instance(self):
         base_instance = Base()
         self.assertIsInstance(base_instance, Base)
@@ -36,10 +38,54 @@ class TestBase(unittest.TestCase):
         base_instance = Base(5)
         self.assertEqual(base_instance.id, 5)
 
+    def test_to_json_string_empty_list(self):
+        result = Base.to_json_string([])
+        self.assertEqual(result, "[]")
+
+    def test_to_json_string_with_none(self):
+        result = Base.to_json_string(None)
+        self.assertEqual(result, "[]")
+
+    def test_to_json_string_with_dictionaries(self):
+        input_list = [{"key1": "value1", "key2": "value2"}, {"key3": "value3"}]
+        result = Base.to_json_string(input_list)
+        expected_json = '[{"key1": "value1", "key2": "value2"}, {"key3": "value3"}]'
+        self.assertEqual(result, expected_json)
+
+    def test_to_json_string_nested_list(self):
+        input_list = [{"key1": {"nested_key": "nested_value"}}, {"key2": "value2"}]
+        result = Base.to_json_string(input_list)
+        expected_json = '[{"key1": {"nested_key": "nested_value"}}, {"key2": "value2"}]'
+        self.assertEqual(result, expected_json)
+
+
     def test_to_json_string(self):
         base_instance = Base(1)
         json_string = base_instance.to_json_string([{'id': 1, 'x': 2, 'y': 3}])
         self.assertEqual(json_string, '[{"id": 1, "x": 2, "y": 3}]')
+
+
+    def test_save_to_file_empty_list(self):
+        Base.save_to_file([])
+        file_name = f"Base.json"
+        with open(file_name, 'r', encoding='utf-8') as f:
+            content = f.read()
+            self.assertEqual(content, "[]")
+        # Clean up: Delete the created file
+        os.remove(file_name)
+
+    def test_save_to_file_with_objects(self):
+        obj1 = Rectangle(4, 3)
+        obj2 = Rectangle(5, 7)
+        #Rectangle.save_to_file([obj1, obj2])
+        #file_name = f"Rectangle.json"
+
+        #with open(file_name, 'r', encoding='utf-8') as f:
+        #    content = f.read()
+        #    expected_json = '[{"x": 0, "y": 0, "id": 6, "height": 3, "width": 4}, {"x": 0, "y": 0, "id": 7, "height": 7, "width": 5}]'
+        #    self.assertEqual(content, expected_json)
+        # Clean up: Delete the created file
+        # os.remove(file_name)
 
     def test_save_to_file(self):
         r1 = Rectangle(5, 3)
@@ -62,6 +108,17 @@ class TestBase(unittest.TestCase):
 
         for loaded_rect, original_rect in zip(loaded_rectangles, rectangles):
             self.assertEqual(loaded_rect.to_dictionary(), original_rect.to_dictionary())
+
+    def test_load_from_file_csv_empty(self):
+        # Test loading from an empty CSV file
+        file_name = f"Rectangle.csv"
+        with open(file_name, 'w', encoding='utf-8') as f:
+            pass  # Create an empty CSV file
+
+        result = Rectangle.load_from_file_csv()
+
+        self.assertIsInstance(result, list)
+        self.assertEqual(len(result), 0)
 
 class TestRectangle(unittest.TestCase):
 
