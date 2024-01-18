6bde2e8e97fc458ad606ee8645fc07b3fb57a618
AlexDelei <alexdelei125@gmail.com>
2024-01-17 21:10:28 -0800

    create user, grants previliges, join db and tables

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
 -- creates a user who exists
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
