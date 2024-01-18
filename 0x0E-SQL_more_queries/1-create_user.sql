6a220b9e8f9724a70366ee7f0a3efcd869d1edb8
AlexDelei <alexdelei125@gmail.com>
2024-01-17 21:06:17 -0800

    create user, grants previliges, join db and tables

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- creates a user who exists
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,4 @@
+SQL Queries:
+1. Creating User
+2. Granting Access to User
+3. JOINING Tables and Databases
