1a7ad47dfda9b75b35fb3dac8aa6a1e647b94466
AlexDelei <alexdelei125@gmail.com>
2024-01-17 22:26:54 -0800

    create user, grants previliges, join db and tables

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,10 @@
--- creating a database and a user
+-- Creating a database and a user
 
+
+-- Creating the database even if it exists
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Creating user if does not exists and grants privileges
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT  ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
