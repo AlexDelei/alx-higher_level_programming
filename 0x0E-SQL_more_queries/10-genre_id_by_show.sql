1200c28c8b35b052d297b0ae19038184f58e115a
AlexDelei <alexdelei125@gmail.com>
2024-01-18 06:28:38 -0800

    create user, grants previliges, join db and tables

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,3 @@
+-- imports dump db from hbtn_0d_tvshow
+
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
