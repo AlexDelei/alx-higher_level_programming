256fab54ecc1f477e92574add78406855ee48aa8
AlexDelei <alexdelei125@gmail.com>
2024-01-18 19:54:36 -0800

    create user, grants previliges, join db and tables

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,11 @@
+-- listing everything
+
+SELECT tv_genres.name
+FROM tv_genres
+WHERE tv_genres.id NOT IN (
+    SELECT tv_show_genres.genre_id
+    FROM tv_show_genres
+    JOIN tv_shows ON tv_show_genres.show_id = shows.id
+    WHERE tv_shows.title = 'Dexter'
+)
+ORDER BY tv_genres.name ASC;
