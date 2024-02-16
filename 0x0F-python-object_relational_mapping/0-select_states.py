24a9964cd741d5930b570e39a83db6e21e56a7fe
AlexDelei <alexdelei125@gmail.com>
2024-02-16 12:25:07 -0800

    Python ORMS and mysql

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,14 +4,15 @@ Lets list everything in our new database
 """
 import MySQLdb
 
-db = MySQLdb.connect(username="", password="", database="hbtn_0e_0_usa")
+db = MySQLdb.connect(user="root", password="", database="hbtn_0e_0_usa")
 all_items = db.cursor()
 all_items.execute(
         "SELECT * FROM states "
         "ORDER BY states.id ASC"
         )
 result = all_items.fetchall()
-db.close()
 
-for i in result:
-    print(i)
+
+if __name__ == '__main__':
+    for i in result:
+        print(i)
