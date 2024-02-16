a83d00ebe11a3c2ba7cf1c62d133da10c399a81e
AlexDelei <alexdelei125@gmail.com>
2024-02-16 13:55:24 -0800

    Python ORMS and mysql

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+listing according to science
+"""
+import MySQLdb
+import sys
+
+
+def list_caes(username, password, database):
+    """"list view wothout restrications"""
+    db = MySQLdb.connect(user=username, password=password, database=database)
+    query = db.cursor()
+    query.execute(f"SELECT * FROM states WHERE name LIKE 'N%'")
+
+    result = query.fetchall()
+
+    for i in result:
+        print(i)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print("Usage: executable.py <username> <password> <database>")
+        sys.exit(1)
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    list_caes(username, password, database)
