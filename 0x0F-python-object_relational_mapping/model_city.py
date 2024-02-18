e4fd827743a297e2d5c83b6501eca40e2ca09e5d
AlexDelei <alexdelei125@gmail.com>
2024-02-18 05:28:33 -0800

    Python ORMS and mysql

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -17,8 +17,8 @@ class City(Base):
     """
     __tablename__ = 'cities'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'))
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
 
     state = relationship("State", back_populates="cities")
