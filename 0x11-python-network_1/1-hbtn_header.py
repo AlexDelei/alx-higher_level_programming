7896c495139b83ccccf3a3511ce07fa7c49c3344
AlexDelei <alexdelei125@gmail.com>
2024-03-01 21:24:57 -0800

    fetching id from headers in a url request

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,9 +5,9 @@ Sending a request to the URL and display value of id found in the header
 import sys
 import urllib.request
 
-url = sys.argv[1]
 
 if __name__ == '__main__':
+    url = sys.argv[1]
     with urllib.request.urlopen(url) as resp:
         headers = resp.headers
         print(headers['X-Request-Id'])
